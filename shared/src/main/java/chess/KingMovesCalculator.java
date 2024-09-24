c3ce54b1d7a41b21f68105b9087c8e78ec67dff5
parkysparky <sp474@byu.edu>
2024-09-24 15:42:33 -0600

    Passing King tests

--- shared/src/main/java/chess/KingMovesCalculator.java
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -8,7 +8,17 @@ public class KingMovesCalculator implements PieceMoveCalculator{
     public HashSet<ChessMove> moveCalculator(ChessBoard board, ChessPosition position) {
         HashSet<ChessMove> moves = new HashSet<>();
 
-
+        for(int i = position.getColumn()-1; i <= position.getColumn()+1; i++){
+            for( int j = position.getRow()-1; j <= position.getRow()+1; j++){
+                ChessPosition endPosition = new ChessPosition(j, i);
+                if(isInBounds(endPosition)){
+                    if(isEmpty(board, endPosition) || canCapture(board, position,endPosition)) {
+                        moves.add(new ChessMove(position, endPosition, null));
+                    }
+                    if(position.equals(endPosition)){ moves.remove(endPosition); }
+                }
+            }
+        }
 
         return moves;
     }
